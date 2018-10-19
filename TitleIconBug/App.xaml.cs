37ba289697b9e1141b391cb4b75f316d23a4db98
Peter Major <petermajoruk@gmail.com>
2018-10-19 19:44:44 +0100

    MasterDetail page with icon

--- TitleIconBug/App.xaml.cs
+++ TitleIconBug/App.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using TitleIconBug.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace TitleIconBug
--- TitleIconBug/MainPage.cs
+++ TitleIconBug/MainPage.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Xamarin.Forms;
+
 namespace TitleIconBug
 {
-    public class MainPage
+    public class MainPage : MasterDetailPage
     {
         public MainPage()
         {
+            MasterBehavior = MasterBehavior.Popover;
+
+            Master = new ContentPage { Title = "Menu" };
+
+            var detailPage = new ContentPage();
+            NavigationPage.SetTitleIcon(detailPage, "my_icon");
+
+            Detail = new NavigationPage(detailPage);
         }
     }
 }
